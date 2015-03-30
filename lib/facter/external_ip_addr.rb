52ca940474f7c535ad827443344258287d84fd69
Douglas Greenbaum <dggreenbaum@greenbad.org>
2015-03-29 22:26:36 -0700

    Now putting some validation back.

--- lib/facter/external_ip_addr.rb
+++ lib/facter/external_ip_addr.rb
@@ -16,5 +16,21 @@ require 'facter/util/macaddress'
 # Make a fact for the ip address the rest of the world thinks we have.
 Facter.add("external_ip_addr") do
   confine :kernel => 'Linux'
-  setcode `/usr/bin/curl ifconfig.me/ip`
+  setcode do
+    ip = nil
+    tmp = ''
+    output = `/usr/bin/curl -s ifconfig.me/ip`
+    result = $?.success?
+    if output and result
+      regexp = /([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\/[0-9]{1,2})/
+      output.each_line do |line|
+        match = regexp.match(line)
+        if match
+          tmp = "#{tmp},#{match[1]}"
+        end
+      end
+      ip = tmp[1..-1]
+    end
+    ip
+  end
 end
